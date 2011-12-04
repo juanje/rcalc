f74ee0d2d86721293b335842c04b473c48609b91
Juanje Ojeda <jojeda@emergya.es>
2011-12-04 23:07:24 +0100

    Updated the previous features to use the new bin/rcalc tool

--- features/step_definitions/calculator_steps.rb
+++ features/step_definitions/calculator_steps.rb
@@ -1,12 +1,20 @@
-Given /^the input "([^"]*)"$/ do |input|
-  @input = input
+Given /^the number (\d+) and (\d+)$/ do |first, second|
+  @first = first
+  @second = second
 end
 
-When /^the calculator is run$/ do
-  @output = `ruby calc.rb #{@input}`
+When /^the calculator divide the (\d+) by the (\d+)$/ do |first, second|
+  @output = `ruby bin/rcalc divide #{@first} #{@second}`.chomp
   raise('Command failed!') unless $?.success?
 end
 
+When /^the calculator sum them$/ do
+  @output = `ruby bin/rcalc sum #{@first} #{@second}`.chomp
+  raise('Command failed!') unless $?.success?
+end
+
+# Common output check
 Then /^the output should be "([^"]*)"$/ do |expected_output|
   @output.should == expected_output
 end
+
